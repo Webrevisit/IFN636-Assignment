a1b7429710f1a36c4df6cf4c6418579af26b7145
Shino <shinopadiyara@gmail.com>
2026-04-15 23:05:03 +0000

    Updating public ip for demo

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://54.252.192.241:5001', // live
+  baseURL: 'http://3.106.244.238:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
